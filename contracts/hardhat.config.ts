7e29bb22318fc5ae16871032f39e62dacb1b73d5
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2025-10-11 14:00:22 +0530

    Initial setup of project files and directory structure.

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,29 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import hardhatVerifyPlugin from "@nomicfoundation/hardhat-verify";
+import "dotenv/config";
+
+const { ALCHEMY_API_KEY, WALLET_KEY, ETHERSCAN_API_KEY } = process.env;
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxViemPlugin, hardhatVerifyPlugin],
+  solidity: {
+    version: "0.8.28",
+  },
+  networks: {
+    arbitrumSepolia: {
+      type: "http",
+      url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [WALLET_KEY || ""],
+      chainId: 421614,
+    },
+  },
+  verify: {
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  },
+};
+
+export default config;
